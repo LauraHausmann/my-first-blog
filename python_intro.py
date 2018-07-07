3d28179f765cf9cc6b8ef8d4955e8cb5290c5fe9
Laura <du@beispiel.com>
2018-07-07 14:58:24 +0200

    Meine Django Girls App, erster Commit

---
+++ python_intro.py
@@ -0,0 +1,48 @@
+# print("Hello, Django girls!")
+# print(3 + 5)
+# if 3 > 2: 
+#     print("it works!")
+# if 5 > 2:
+#     print("five is indeed greater than two")
+# else:
+#     print("5 is not greater than 2")
+# name = "Sonja"
+# if name == "Laura":
+#     print("Hallo Laura")
+# elif name == "Sonja":
+#     print("Hallo Sonja")
+# else:
+#     print("Hallo Du! Wer bist du?")
+# def hi():
+#     print("Hallo!")
+#     print("wie gehts dir?")
+
+# hi()
+# def hi(name):
+#     if name == "Laura":
+#         print("Hey Laura!")
+#     elif name == "Sonja":
+#         print("Hey Sonja!")
+#     else:
+#         print("Hallo du! Wer bist du?")
+
+# hi("Lisa")
+# def hi(name):
+#     print("Hi " + name + "!")
+
+# hi ("Rachel")
+
+
+# # ab hier geht mein code los
+# def hi(name):
+#     print("Hi " + name + "!")
+
+# # Mädchen = ("Lisa", "Laura", "Sonja", "Dasha", "Rachel")
+# # for name in Mädchen:
+# #     hi(name)
+# #     print("Nächstes Mädchen")
+
+# hi("Laura")
+
+for i in range(1, 6):
+    print(i)
